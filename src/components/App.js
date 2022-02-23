c832cbfa3ebe248595b2049b44ad844389a4e9c4
Ibaihima <ibaihima@hotmail.com>
2022-02-23 06:37:49 -0500

    commit tree 75caafbfee043f3b538fa1e3cf9bb399650d5969

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,17 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+let About = () =><div id = "about"><h2>About</h2></div>
+
+
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
